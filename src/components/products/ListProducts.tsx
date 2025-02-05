08cad1ee2100cf9703e8288b25c562ed1cea9a3f
María Rodriguez <mariarodriguez@Marias-MacBook-Air.local>
2025-02-05 13:04:33 -0600

    feature: folder structure and router

---
+++ src/components/products/ListProducts.tsx
@@ -0,0 +1,9 @@
+const ListProducts = () => {
+  return (
+    <div>
+      <h1>List of Products</h1>
+    </div>
+  )
+}
+
+export { ListProducts }
---
+++ src/views/home/HomeView.tsx
@@ -0,0 +1,10 @@
+const HomeView = () => {
+  return (
+    <div>
+      <h1>Home View</h1>
+      <p>This is the Home View</p>
+    </div>
+  )
+}
+
+export { HomeView }
