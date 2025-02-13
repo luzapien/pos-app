9179808d22d038c53ccffd9b7acfb89540f3dfba
María Rodíguez <lu.rzapien.92@gmail.com>
2025-02-13 12:58:26 -0600

    refactor: Category type file

--- src/components/products/Form.tsx
+++ src/components/products/Form.tsx
@@ -4,7 +4,7 @@ import { productSchema, type Product } from '@/schemas/product'
 import { Button, Form, Input, Select, SelectItem } from '@heroui/react'
 import { zodResolver } from '@hookform/resolvers/zod'
 import { useForm } from 'react-hook-form'
-import type { Category } from '@/types/products'
+import type { Category } from '@/types/categories'
 
 export const ProductsForm = () => {
   const form = useForm<Product>({
@@ -30,35 +30,36 @@ export const ProductsForm = () => {
   }, [])
 
   return (
-    <Form className="w-full max-w-xs" onSubmit={onSubmit}>
-      <Input label="Name" placeholder="Ex. Milk" {...form.register('name')} isRequired />
-      {errors.name && <span>{errors.name.message}</span>}
-      <Select
-        isRequired
-        className="max-w-xs"
-        label="Category"
-        {...form.register('category', { required: true })}
-        placeholder="Select a Category"
-      >
-        {newCategoriesValue.map((newValue) => (
-          <SelectItem key={newValue.key}>{newValue.label}</SelectItem>
-        ))}
-      </Select>
-      {errors.category && <span>This field is required</span>}
-      <Input label="Packaging" placeholder="Ex. 1 L bottle" {...form.register('packaging')} />
-      {errors.name && <span>This field is required</span>}
-      <Input
-        type="number"
-        label="Price"
-        placeholder="0.00"
-        startContent={
-          <div className="pointer-events-none flex items-center">
-            <span className="text-default-400 text-small">$</span>
-          </div>
-        }
-        {...form.register('price')}
-      />
-      {errors.price && <span>{errors.price.message}</span>}
+    <Form onSubmit={onSubmit}>
+      <div className=" w-full grid grid-cols-2 gap-12">
+        <Input label="Name" placeholder="Ex. Milk" {...form.register('name')} isRequired />
+        {errors.name && <span>{errors.name.message}</span>}
+        <Select
+          isRequired
+          label="Category"
+          {...form.register('category', { required: true })}
+          placeholder="Select a Category"
+        >
+          {newCategoriesValue.map((newValue) => (
+            <SelectItem key={newValue.key}>{newValue.label}</SelectItem>
+          ))}
+        </Select>
+        {errors.category && <span>This field is required</span>}
+        <Input label="Packaging" placeholder="Ex. 1 L bottle" {...form.register('packaging')} />
+        {errors.name && <span>This field is required</span>}
+        <Input
+          type="number"
+          label="Price"
+          placeholder="0.00"
+          startContent={
+            <div className="pointer-events-none flex items-center">
+              <span className="text-default-400 text-small">$</span>
+            </div>
+          }
+          {...form.register('price')}
+        />
+        {errors.price && <span>{errors.price.message}</span>}
+      </div>
       <Button type="submit" variant="bordered">
         Save
       </Button>
